ff646189c5c85186b55ddc9bde9b18fe25ff7991
Burke Holland <burkeholland@gmail.com>
2012-07-14 16:48:36 -0500

    initial commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,56 +1,23 @@
-// 
-//  --- our app behavior logic ---
-//
-run(function () {
-    // immediately invoked on first run
-    var init = (function () {
-        if (navigator.network.connection.type == Connection.NONE) {
-            alert("No internet connection - we won't be able to show you any maps");
-        } else {
-            alert("We can reach Google - get ready for some awesome maps!");
-        }
-    })();
-    
-    // a little inline controller
-    when('#welcome');
-    when('#settings', function() {
-		// load settings from store and make sure we persist radio buttons.
-		store.get('config', function(saved) {
-			if (saved) {
-				if (saved.map) {
-					x$('input[value=' + saved.map + ']').attr('checked',true);
-				}
-				if (saved.zoom) {
-					x$('input[name=zoom][value="' + saved.zoom + '"]').attr('checked',true);
-				}
-			}
-		});
-	});
-    when('#map', function () {
-        store.get('config', function (saved) {
-            // construct a gmap str
-            var map  = saved ? saved.map || ui('map') : ui('map')
-            ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
-            ,   path = "http://maps.google.com/maps/api/staticmap?center=";
-			
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var location = "" + position.coords.latitude + "," + position.coords.longitude;
-                path += location + "&zoom=" + zoom;
-                path += "&size=250x250&maptype=" + map + "&markers=color:red|label:P|";
-                path += location + "&sensor=false";
+var IMGURER = IMGURER || {};
 
-                x$('img#static_map').attr('src', path);
-            }, function () {
-                x$('img#static_map').attr('src', "assets/img/gpsfailed.png");
-            });
-        });
-    });
-    when('#save', function () {
-        store.save({
-            key:'config',
-            map:ui('map'),
-            zoom:ui('zoom')
-        });
-        display('#welcome');
-    });
-});
+// this function runs at startup and attaches to the 'deviceready' event
+// which is fired by PhoneGap when the hardware is ready for native API
+// calls. It is self invoking and will run immediately when this script file is 
+// loaded.
+(function(application) {
+    if (navigator.userAgent.indexOf('Browzr') > -1) {
+        // blackberry
+        setTimeout(application, 250)	
+    } else {
+        // attach to deviceready event, which is fired when phonegap is all good to go.
+        $(document).bind('deviceready', IMGURER.run);
+    }
+})();
+
+IMGURER.run = function() {
+
+    var app = new kendo.mobile.Application(document.body);
+
+    $("#imgur-key-request").data("kendoMobileModalView").open();
+
+};
